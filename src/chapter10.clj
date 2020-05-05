4fd0ded220c86b7f1bb1dd11de925c3118f6331f
GojeeApps <jack@ducats.net.au>
2020-05-05 18:58:29 +1000

    Alains comments & Feedback

--- src/chapter10.clj
+++ src/chapter10.clj
@@ -16,12 +16,12 @@
 
 ;----------------------------------------------------------------------------------------------------------------------------------
 ;;;; Excercise 2:
-;; Create a function that uses futures to parallelize the task of downloading random quotes from 
-;; https://www.braveclojure.com/random-quote using (slurp "https://www.braveclojure.com/random-quote"). 
-;; The futures should update an atom that refers to a total word count for all quotes. The function will take the number of quotes 
-;; to download as an argument and return the atom’s final value. Keep in mind that you’ll need to ensure that all futures have 
+;; Create a function that uses futures to parallelize the task of downloading random quotes from
+;; https://www.braveclojure.com/random-quote using (slurp "https://www.braveclojure.com/random-quote").
+;; The futures should update an atom that refers to a total word count for all quotes. The function will take the number of quotes
+;; to download as an argument and return the atom’s final value. Keep in mind that you’ll need to ensure that all futures have
 ;; finished before returning the atom’s final value. Here’s how you would call it and an example result:
-;; 
+;;
 ;; (quote-word-count 5)
 ;; => {"ochre" 8, "smoothie" 2}
 ;----------------------------------------------------------------------------------------------------------------------------------
@@ -36,19 +36,58 @@
   [n-quotes]
   (doseq [calls (partition-all (/ n-quotes n-cores) (range n-quotes))] ; this splits up the input up into n new threads, being equal to the number of cores on my machine.
     (doseq [nth-call calls]
-      (let [read-quote (promise)]
-        (future (deliver read-quote (slurp url))
-                (swap! word-freq (fn [current-state]
-                                   (merge-with + current-state (frequencies (s/split @read-quote #"\W+"))))))
-        @read-quote)))
+;;; AP - This has a subtle, and nasty bug. :-(
+;;; You're blocking on @read-quote right after each future,
+;;; and thus the doseq can't start the next future until each
+;;; one has finished.  You can see this by inserting a print statement
+;;; in your future like I've done below.  On my box, this printed:
+;;;
+;; Starting future: 16
+;; Finished future:  16
+;; Starting future: 200
+;; Finished future:  200
+;; Starting future: 951
+;; Finished future:  951
+;; Starting future: 333
+;; Finished future:  333
+;; Starting future: 322
+;; Finished future:  322
+;;
+;; You need to get to a situation where it prints something more like:
+;; Starting future: 16
+;; Starting future: 200
+;; Starting future: 322
+;; Starting future: 333
+;; Starting future: 951
+;; Finished future:  16
+;; Finished future:  200
+;; Finished future:  322
+;; Finished future:  333
+;; Finished future:  951
+;;
+;; Do you understand why?  AP
+      (let [read-quote (promise)
+            id (rand-int 1000)]
+        (future
+          (println "Starting future:" id)
+          (deliver read-quote (slurp url))
+          (swap! word-freq (fn [current-state]
+                             (merge-with + current-state (frequencies (s/split @read-quote #"\W+"))))))
+        @read-quote
+        (println "Finished future: " id))))
+;;; AP - Try to not return global values from functions.
+;;; Better is:
+;;; (let [word-freq (atom {})]
+;;;   (your code here)
+;;;   @word-freq)
   @word-freq)
 
 (quote-word-count 5)
 
 ;----------------------------------------------------------------------------------------------------------------------------------
 ;;;; Excercise 3:
-;; Create representations of two characters in a game. The first character has 15 hit points out of a total of 40. The second 
-;; character has a healing potion in his inventory. Use refs and transactions to model the consumption of the healing potion and 
+;; Create representations of two characters in a game. The first character has 15 hit points out of a total of 40. The second
+;; character has a healing potion in his inventory. Use refs and transactions to model the consumption of the healing potion and
 ;; the first character healing.
 ;----------------------------------------------------------------------------------------------------------------------------------
 
@@ -72,6 +111,9 @@
 
 (defn is-dead?
   [{:keys [hit]}]
+  ;; AP - Stylistic note: typically, a function with a name ending in a question mark
+  ;; is considered a "predicate" and returns a true/false value.  It should never throw.
+  ;; I would call this function something like `assert-alive!`
   (or (< hit 40)
       (throw (IllegalStateException. "You're dead! You have taken too many hits.. :("))))
 
@@ -84,5 +126,4 @@
   (dosync
    (alter player update-in [:hit] + hit)))
 
-(take-hit player-3 40)
-
+(take-hit player-3 10)
--- src/chapter9.clj
+++ src/chapter9.clj
@@ -3,7 +3,7 @@
 
 ;----------------------------------------------------------------------------------------------------------------------------------
 ;;;; Excercises 1 and 2:
-;; 1. Write a function that takes a string as an argument and searches for it on Bing and Google using the slurp function. 
+;; 1. Write a function that takes a string as an argument and searches for it on Bing and Google using the slurp function.
 ;; Your function should return the HTML of the first page returned by the search.
 ;; 2. Update your function so it takes a second argument consisting of the search engines to use.
 ;----------------------------------------------------------------------------------------------------------------------------------
@@ -19,15 +19,27 @@
        (future (deliver read-engine (slurp (str engine search-term)))
                (println @read-engine))))))
 
-(online-search "clojure")
+;; AP - This appears to not return any value?  How do you get the results out?
 
-;; ** bug - noticed that when you call the function with just one search engine (not a list of engines), it has an error as it then 
+(online-search "clojure") ;; => nil
+
+
+;; ** bug - noticed that when you call the function with just one search engine (not a list of engines), it has an error as it then
 ;; splits the one input out into characters in the doseq.
 ;; Icouldnt quickly figure out how to resolve this without using if statements etc. Is there an easy way to do this?
 
+;; AP - Your function signature is [search-term search-engines] so you need to
+;; provide exactly 2 arguments.  So call it like this:
+(online-search "clojure" ["https://www.bing.com/search?q%3D" "https://au.search.yahoo.com/search?p%3D"])
+
+;; If you want to call like like this:
+;(online-search2 "clojure" "https://www.bing.com/search?q%3D" "https://au.search.yahoo.com/search?p%3D")
+;; You need to redefine the signagure as being [search-term & search-engines]
+;; The & will wrap all the remaining arguments into a sequence.
+
 ;----------------------------------------------------------------------------------------------------------------------------------
 ;;;; Excercise 3
-;; 1. Create a new function that takes a search term and search engines as arguments, and returns a vector of the URLs from the 
+;; 1. Create a new function that takes a search term and search engines as arguments, and returns a vector of the URLs from the
 ;; first page of search results from each search engine.
 ;----------------------------------------------------------------------------------------------------------------------------------
 
@@ -36,4 +48,3 @@
   (re-seq #"href=\"(https?://[=?.:/\w]+)" source))
 
 ;; not finished... but the regex above was tested - thanks Alain.
-
